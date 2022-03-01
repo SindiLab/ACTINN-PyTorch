f846f53cc15576ac5fbd2023ce2075e452972f9f
dr-aheydari <aliheydari@ucdavis.edu>
2022-03-01 22:54:25 +0000

    added options for training with highly-variable-gene data + transfer learning

---
+++ run_ACTINN_TransferLearning.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 python classify.py --ClassifierEpochs 1 --workers 12 --pretrained /home/jovyan/SindiLab_GitHub/ACTINN/ClassifierWeights/LASTmodel_epoch_50_iter_0.pth --reset_epochs 
